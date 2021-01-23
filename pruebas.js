10ccbf05eb7e0b2e6510e9832b73eff06981defa
David Rodriguez Prada <david_rprada@yahoo.es>
2021-01-23 21:26:50 +0100

    chore: fix .gitignore file

--- pruebas.js
+++
@@ -1,6 +0,0 @@
-// Importar modulos
-const cron = require('node-cron'); // Programdor de tareas temporizables
- 
-cron.schedule('* * * * * *', async () => {
-  console.log('running a task every minute');
-});
